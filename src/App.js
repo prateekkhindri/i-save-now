c7306a16df48b9c5c284cf48c4d39ec3934c92de
PrateekKhindri <prateek.khindri90@gmail.com>
2023-09-08 13:01:54 +1000

    Added loader for when the page loads

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import "./App.css";
 import ComingSoon from "./pages/coming-soon/ComingSoon";
+import logo from "./assets/iSaveNow-Logos-RGB_Logo-1.png";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <ComingSoon />
+      {loading ? (
+        <div className="fixed inset-0 flex items-center justify-center bg-[#0A2844] z-50">
+          <img src={logo} alt="Loading..." className="w-32 md:w-64" />
+        </div>
+      ) : (
+        <ComingSoon />
+      )}
     </div>
   );
-}
+};
 
 export default App;
